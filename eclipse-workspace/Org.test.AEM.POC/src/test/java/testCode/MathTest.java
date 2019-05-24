f508f53fe009dec535998ca5ca7464f49583f8cf
S.GAMPA <S.GAMPA@HYD-L-00062414.olympus.F5Net.com>
2019-05-24 15:28:54 +0530

    code coverage failure check

--- eclipse-workspace/Org.test.AEM.POC/src/test/java/testCode/MathTest.java
+++ eclipse-workspace/Org.test.AEM.POC/src/test/java/testCode/MathTest.java
@@ -15,7 +15,7 @@ public class MathTest {
 		Assert.assertEquals(obj.getAbsVal(-1), 1);
 	}
 	
-	@Test
+	/*@Test
 	public void unitTest2()
 	{
 		MathCode obj = new MathCode();
@@ -27,6 +27,5 @@ public class MathTest {
 	{
 		MathCode obj = new MathCode();
 		Assert.assertEquals(obj.getAbsVal(2), 2);
-		System.out.println("Test with Anupama!!!");
-	}
+	}*/
 }
